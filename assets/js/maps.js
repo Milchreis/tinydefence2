e34cf168673c0389c4b3f5a9ebf67e33710c99ca
MilchReis <nick-m@gmx.de>
2018-03-15 23:29:07 +0100

    balancing the first level

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -3,29 +3,27 @@ var tinydefence = tinydefence || {};
 
 tinydefence.maps = [
     {
+        // Key for this map
         key: 'level1',
+        // Path to map data
         data: 'assets/maps/map1.json',
+        // Readable name for this map/level
         name: 'Level 1',
+        // Start point for the enemies
         start: {x: 0, y: 1},
+        // End point for the enemies
         end: {x: 16, y: 14},
+        // Waves
         waves: [
-            {dropInMillis: 1500, maxEnemies: 1,  enemyHealth: 10, enemySpeed: 10, points: 5},
-            {dropInMillis: 1500, maxEnemies: 2,  enemyHealth: 50, enemySpeed: 20, points: 5},
-            {dropInMillis: 2000, maxEnemies: 5,  enemyHealth: 50, enemySpeed: 20, points: 5},
-            {dropInMillis: 1500, maxEnemies: 2,  enemyHealth: 50, enemySpeed: 20, points: 5},
-            {dropInMillis: 1000, maxEnemies: 10, enemyHealth: 50, enemySpeed: 20, points: 5},
-            {dropInMillis: 1000, maxEnemies: 15, enemyHealth: 30, enemySpeed: 50, points: 5},
-            {dropInMillis: 800,  maxEnemies: 20, enemyHealth: 60, enemySpeed: 20, points: 5},
+            {dropInMillis: 1500, maxEnemies: 2,  enemyHealth: 10, enemySpeed: 10, points: 5},
+            {dropInMillis: 1500, maxEnemies: 4,  enemyHealth: 20, enemySpeed: 20, points: 5},
+            {dropInMillis: 2000, maxEnemies: 6,  enemyHealth: 20, enemySpeed: 30, points: 5},
+            {dropInMillis: 1500, maxEnemies: 8,  enemyHealth: 20, enemySpeed: 30, points: 5},
+            {dropInMillis: 1000, maxEnemies: 10, enemyHealth: 20, enemySpeed: 30, points: 5},
+            {dropInMillis: 1000, maxEnemies: 15, enemyHealth: 20, enemySpeed: 30, points: 5},
+            {dropInMillis: 800,  maxEnemies: 20, enemyHealth: 20, enemySpeed: 30, points: 5},
+            // Add more waves here
         ]
     },
-
-    // {
-    //     name: 'Level 2',
-    //     // Currently only straight lines are supported 
-    //     waypoints: [[,], [,], [,]],
-    //     waves: [
-    //         // enemyDropTimeInMillis, maxEnemies, enemyHealthOffset, enemySpeedOffset, pointOffset
-    //         [],
-    //     ]
-    // },
+    // Add more maps here
 ];
