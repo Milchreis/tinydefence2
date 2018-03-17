d83c365d3f2cb4c4e8e7e2eac686e5f0f74aa2f8
naofireblade <arne.blumentritt@gmail.com>
2018-03-17 21:10:08 +0100

    Added json, Changed train map to default map

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -5,6 +5,31 @@ tinydefence.maps = [
     
     {
         // Key for this map
+        key: 'train',
+        // Path to map data
+        data: 'assets/maps/Train/map.json',
+        // Path to map sprite
+        sprite: 'assets/maps/Train/map.png',
+        // Readable name for this map/level
+        name: 'Train',
+        // Start point for the enemies
+        start: {x: 8, y: 2},
+        // End point for the enemies
+        end: {x: 1, y: 13},
+        // Waves
+        waves: [
+            {dropInMillis: 1500, maxEnemies: 2,  enemyHealth: 10, enemySpeed: 10, points: 5, type: 'enemy'},
+            {dropInMillis: 1500, maxEnemies: 4,  enemyHealth: 20, enemySpeed: 20, points: 5, type: 'enemy'},
+            {dropInMillis: 2000, maxEnemies: 6,  enemyHealth: 20, enemySpeed: 30, points: 5, type: 'enemy'},
+            {dropInMillis: 1500, maxEnemies: 8,  enemyHealth: 20, enemySpeed: 30, points: 5, type: 'enemy'},
+            {dropInMillis: 1000, maxEnemies: 10, enemyHealth: 20, enemySpeed: 30, points: 5, type: 'enemy'},
+            {dropInMillis: 1000, maxEnemies: 15, enemyHealth: 20, enemySpeed: 30, points: 5, type: 'enemy'},
+            {dropInMillis: 800,  maxEnemies: 20, enemyHealth: 20, enemySpeed: 30, points: 5, type: 'enemy'},
+            // Add more waves here
+        ]
+    },
+    {
+        // Key for this map
         key: 'level1',
         // Path to map data
         data: 'assets/maps/Level1/map.json',
