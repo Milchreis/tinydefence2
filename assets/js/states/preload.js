09579b315139ebd9d37dc9afd93bbe3fdb573967
naofireblade <arne.blumentritt@gmail.com>
2018-03-17 10:48:23 +0100

    Removed unused sprites

--- assets/js/states/preload.js
+++ assets/js/states/preload.js
@@ -5,9 +5,6 @@ tinydefence.preload = function(game) {};
 tinydefence.preload.prototype = {
 
 	preload: function() {
-		this.game.load.image('sprites', 'assets/images/sprites.png');
-		this.game.load.image('terrain', 'assets/images/terrain.png');
-		this.game.load.image('way', 'assets/images/way.png');
 		this.game.load.image('logo', 'assets/images/logo.png');
 		this.game.load.image('bullet', 'assets/images/bullet.png');
 		this.game.load.spritesheet('selection', 'assets/images/selection.png', 16, 16);
