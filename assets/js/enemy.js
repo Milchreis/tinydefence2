8c8c68c02434662fd084dcc349d8e62edda2ff0b
Leichtbier <mattis.haenel@googlemail.com>
2018-03-18 14:41:10 +0100

    a test for walking directions

--- assets/js/enemy.js
+++ assets/js/enemy.js
@@ -15,14 +15,21 @@ class Enemy {
         this.game = game;
         this.type = type;
 
+        this.sprite = game.add.sprite(this.waypoints[0][0], this.waypoints[0][1], type);
+        this.animationManager = this.sprite.animations
+
         if(type === 'crab') {
-            this.sprite = game.add.sprite(this.waypoints[0][0], this.waypoints[0][1], 'crab');
-            this.animation = this.sprite.animations.add('idle', [0, 1, 2, 3], 8, true);
+            this.animationManager.add('walkLeft', [4, 5, 6, 7], 8, true);
+            this.animationManager.add('walkRight', [0, 1, 2, 3], 8, true);
+            this.animationManager.add('walkUp', [12, 13, 14, 15], 8, true);
+            this.animationManager.add('walkDown', [8, 9, 10, 11], 8, true);
         } else {
-            this.sprite = game.add.sprite(this.waypoints[0][0], this.waypoints[0][1], 'enemy');
-            this.animation = this.sprite.animations.add('idle', [0, 1, 2, 3], 8, true);
+            this.animationManager.add('walkLeft', [0, 1, 2, 3], 8, true);
+            this.animationManager.add('walkRight', [0, 1, 2, 3], 8, true);
+            this.animationManager.add('walkUp', [0, 1, 2, 3], 8, true);
+            this.animationManager.add('walkDown', [0, 1, 2, 3], 8, true);
         }
-        
+
         this.game.physics.enable(this.sprite, Phaser.Physics.ARCADE);
         this.sprite.anchor.x = 0.5;
         this.sprite.anchor.y = 0.5;
@@ -30,11 +37,36 @@ class Enemy {
         this.sprite.scale.setTo(tinydefence.scalefactor, tinydefence.scalefactor);
 
         this.sprite.health = this.maxhealth;
-        this.sprite.animations.play('idle');
+        //this.sprite.animations.play('idle');
 
         this.graphics = this.game.add.graphics(0, 0);
+        this.velocityOld = {}
     }
-    
+
+    walkLeft() {
+        this.sprite.body.velocity.x = -this.speed;
+        this.sprite.body.velocity.y = 0;
+        this.animationManager.play('walkLeft');
+    }
+
+    walkRight() {
+        this.sprite.body.velocity.x = this.speed;
+        this.sprite.body.velocity.y = 0;
+        this.animationManager.play('walkRight');
+    }
+
+    walkDown() {
+        this.sprite.body.velocity.x = 0;
+        this.sprite.body.velocity.y = this.speed;
+        this.animationManager.play('walkDown');
+    }
+
+    walkUp() {
+        this.sprite.body.velocity.x = 0;
+        this.sprite.body.velocity.y = -this.speed;
+        this.animationManager.play('walkUp');
+    }
+
     update() {
         this.graphics.clear();
 
@@ -51,6 +83,9 @@ class Enemy {
 
         // Next waypoint reached?
         if(this.inRange(x, this.nextWaypoint[0], 3) && this.inRange(y, this.nextWaypoint[1], 3)) {
+            this.sprite.body.x = this.nextWaypoint[0]
+            this.sprite.body.y = this.nextWaypoint[1]
+
             this.waypointIndex += 1;
             // last waypoint reached?
             if(this.waypointIndex >= this.waypoints.length) {
@@ -66,19 +101,15 @@ class Enemy {
         } else {
             // Move to waypoint
             if(x < this.nextWaypoint[0]) {
-                this.sprite.body.velocity.x = this.speed;
+                this.walkRight();
             } else if(x > this.nextWaypoint[0]) {
-                this.sprite.body.velocity.x = -this.speed;
-            } else {
-                this.sprite.body.velocity.x = 0;
+                this.walkLeft();
             }
 
             if(y < this.nextWaypoint[1]) {
-                this.sprite.body.velocity.y = this.speed;
+                this.walkDown();
             } else if(y > this.nextWaypoint[1]) {
-                this.sprite.body.velocity.y = -this.speed;
-            } else {
-                this.sprite.body.velocity.y = 0;
+                this.walkUp();
             }
         }
 
--- assets/js/maps.js
+++ assets/js/maps.js
@@ -43,13 +43,13 @@ tinydefence.maps = [
         end: {x: 28, y: 10},
         // Waves
         waves: [
-            {dropInMillis: 1500, maxEnemies: 2,  enemyHealth: 10, enemySpeed: 10, points: 5, type: 'crab'},
-            {dropInMillis: 1500, maxEnemies: 4,  enemyHealth: 15, enemySpeed: 20, points: 5, type: 'crab'},
-            {dropInMillis: 2000, maxEnemies: 6,  enemyHealth: 15, enemySpeed: 30, points: 5, type: 'crab'},
-            {dropInMillis: 1500, maxEnemies: 10,  enemyHealth: 15, enemySpeed: 30, points: 5, type: 'crab'},
-            {dropInMillis: 1000, maxEnemies: 10, enemyHealth: 15, enemySpeed: 30, points: 5, type: 'crab'},
-            {dropInMillis: 1000, maxEnemies: 14, enemyHealth: 20, enemySpeed: 30, points: 5, type: 'crab'},
-            {dropInMillis: 800,  maxEnemies: 18, enemyHealth: 20, enemySpeed: 30, points: 5, type: 'crab'},
+            {dropInMillis: 1500, maxEnemies: 2,  enemyHealth: 10, enemySpeed: 20, points: 5, type: 'crab'},
+            {dropInMillis: 1500, maxEnemies: 4,  enemyHealth: 15, enemySpeed: 40, points: 5, type: 'crab'},
+            {dropInMillis: 2000, maxEnemies: 6,  enemyHealth: 15, enemySpeed: 60, points: 5, type: 'crab'},
+            {dropInMillis: 1500, maxEnemies: 10,  enemyHealth: 15, enemySpeed: 60, points: 5, type: 'crab'},
+            {dropInMillis: 1000, maxEnemies: 10, enemyHealth: 15, enemySpeed: 60, points: 5, type: 'crab'},
+            {dropInMillis: 1000, maxEnemies: 14, enemyHealth: 20, enemySpeed: 60, points: 5, type: 'crab'},
+            {dropInMillis: 800,  maxEnemies: 18, enemyHealth: 20, enemySpeed: 60, points: 5, type: 'crab'},
             // Add more waves here
         ]
     },
