202f54a5be6d0f1348aaa085299f4cbf32df59f7
Nick <544436+Milchreis@users.noreply.github.com>
2018-03-29 20:39:52 +0200

    Formatting corrected.

--- assets/js/towermanager.js
+++ assets/js/towermanager.js
@@ -16,20 +16,20 @@ class TowerManager {
 
 	load() {
 		this.towerTypes.forEach(tower => {
-            let towerProperties = this.game.cache.getJSON(tower.key + '_properties');
-            tower.color = towerProperties.color;
-            tower.tiers = towerProperties.tiers;
+		    let towerProperties = this.game.cache.getJSON(tower.key + '_properties');
+		    tower.color = towerProperties.color;
+		    tower.tiers = towerProperties.tiers;
 
-            towerProperties.tiers.forEach((tier, i) => {
-            	// TODO tier specific spritesheets
-            	let path = 'assets/towers/' + tower.key + '/';
-            	tier.spritesheet_tower = tower.key + '_' + i  + '_spritesheet_tower';
-            	tier.spritesheet_shot = tower.key + '_' + i  + '_spritesheet_shot';
+		    towerProperties.tiers.forEach((tier, i) => {
+			// TODO tier specific spritesheets
+			let path = 'assets/towers/' + tower.key + '/';
+			tier.spritesheet_tower = tower.key + '_' + i  + '_spritesheet_tower';
+			tier.spritesheet_shot = tower.key + '_' + i  + '_spritesheet_shot';
 
-                this.game.load.spritesheet(tier.spritesheet_tower, path + tier.sprites.tower, 16, 16);
-                this.game.load.image(tier.spritesheet_shot, path + tier.sprites.shot);
-            });
-        });
+			this.game.load.spritesheet(tier.spritesheet_tower, path + tier.sprites.tower, 16, 16);
+			this.game.load.image(tier.spritesheet_shot, path + tier.sprites.shot);
+		    });
+		});
 	}
 
 	getTowerType(typeName) {
