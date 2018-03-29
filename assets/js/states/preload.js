e924597962cc8ed0fb6957e94fcec27fa6116551
MilchReis <nick-m@gmx.de>
2018-03-29 22:35:12 +0200

    Corrects missing merge from master integration

--- assets/js/states/preload.js
+++ assets/js/states/preload.js
@@ -9,23 +9,13 @@ tinydefence.preload.prototype = {
 		this.game.load.spritesheet('selection', 'assets/images/selection.png', 16, 16);
 		this.game.load.spritesheet('enemy', 'assets/images/enemy.png', 16, 16);
 		this.game.load.spritesheet('crab', 'assets/images/enemyCrab.png', 16, 16);
-<<<<<<< HEAD
-		this.game.load.spritesheet('buttonCoverage', 'assets/images/ui/buttonCoverage.png', 32, 18);
 		
 		this.game.load.image('buildmenu', 'assets/images/ui/menuElements.png');
 		this.game.load.spritesheet('buildmenuButtons', 'assets/images/ui/menuButtons.png', 16, 16);
 
-		this.game.load.spritesheet('Cannon_0_tower', 'assets/towers/Cannon/tower_1.png', 16, 16);
-		this.game.load.image('Cannon_0_shot', 'assets/towers/Cannon/bullet_1.png');
-
-		tinydefence.towers.forEach(tower => {
-			this.game.load.json(tower.key + '_json', 'assets/towers/' + tower.key + '/properties.json');
-		});
-=======
-		this.game.load.spritesheet('buttonCoverage', 'assets/images/buttonCoverage.png', 32, 18);
-		this.game.load.spritesheet('buttonLevel', 'assets/images/buttonLevel.png', 225, 35);
-		this.game.load.spritesheet('buttonMenuNav', 'assets/images/buttonMenuNav.png', 20, 18);
->>>>>>> 165eea34a6e5193ead17b88143ace6017529fcad
+		this.game.load.spritesheet('buttonCoverage', 'assets/images/ui/buttonCoverage.png', 32, 18);
+		this.game.load.spritesheet('buttonLevel', 'assets/images/ui/buttonLevel.png', 225, 35);
+		this.game.load.spritesheet('buttonMenuNav', 'assets/images/ui/buttonMenuNav.png', 20, 18);
 
 		this.game.load.bitmapFont('font_white', 
 			'assets/fonts/font.png',
